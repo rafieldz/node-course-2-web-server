89c41a391146376b4b3f51f7489450978f3d3248
unknown <rflassumpcao@gmail.com>
2018-09-27 07:59:18 -0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+// middleware
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+  next();
+});
+
+app.use('/maintenance', (req, res) => {
+  res.render('maintenance.hbs');
+});
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req,res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Welcome to the Home Page',
+    someText: 'What can we do for you ?',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.get('/api', (req, res) => {
+  res.send({
+    name: 'Rafael',
+    likes: [
+      'Biking',
+      'Cipher'
+    ]
+    });
+  });
+
+app.get('/about', (req,res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+     errorMessage: 'Unable to handle request'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000.');
+});
