d0028bb5aa4e51c812254b8be12a405b1fbc7d70
unknown <rflassumpcao@gmail.com>
2018-09-27 12:51:17 -0300

    updated server.js add /api

--- server.js
+++ server.js
@@ -38,16 +38,16 @@ app.get('/', (req,res) => {
     currentYear: new Date().getFullYear()
   });
 });
-//
-// app.get('/api', (req, res) => {
-//   res.send({
-//     name: 'Rafael',
-//     likes: [
-//       'Biking',
-//       'Cipher'
-//     ]
-//     });
-//   });
+
+app.get('/api', (req, res) => {
+  res.send({
+    name: 'Rafael',
+    likes: [
+      'Biking',
+      'Cipher'
+    ]
+    });
+  });
 
 app.get('/about', (req,res) => {
   res.render('about.hbs', {
